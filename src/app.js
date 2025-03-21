2298b8feba1d7db5a045dd8ad00f69b0153c38a8
Lucky <sashankaswain6@gmail.com>
2025-03-21 19:45:09 +0530

    another type of route handlers

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON request bodies
 
-
-app.use(express.json());
-
-app.get("/user", (req, res) => {
-    res.send({firstName: "John", lastName: "carry"});
+app.get("/user", (req, res, next) => {
+  console.log("handle route handlers");
+  next();
 });
-
-
-
-
+app.get("/user" , (req, res, next) => {
+    console.log("Handle additional route handlers");
+    res.send("User information");
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
