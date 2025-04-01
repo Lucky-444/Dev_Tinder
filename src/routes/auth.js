b017d7bffcc1c095e8e2a6ca805404a8343ce5ca
Lucky <sashankaswain6@gmail.com>
2025-04-01 18:23:26 +0530

    Logout APi

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -77,6 +77,13 @@ authRouter.post("/login", async (req, res) => {
   }); 
 
 
+authRouter.post("/logout", async (req ,res) => {
+  res.cookie("token",null ,{ expires : new Date(Date.now())});
+  res.send("Logged Out");
+  
+  });
+
+
 
 
 
