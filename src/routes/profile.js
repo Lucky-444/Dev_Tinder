ca23f41a301ab75256b1778a0ec16a59b856b860
Lucky <sashankaswain6@gmail.com>
2025-04-01 19:29:52 +0530

    Apis and express router

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const profileRouter = express.Router();
+const { validateEditProfileData  } = require('../utils/validation');
+
 
 
 const {userAuth} = require("../middlewares/auth");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {  
       const user = req.user;
       res.send(user);
@@ -12,6 +14,30 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
       res.status(401).send("You are not authorized to view this profile" + error.message);
     }
   });
-  
 
-module.exports = profileRouter;
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+      if (!validateEditProfileData(req)) {
+        throw new Error("Invalid Edit Request");
+      }
+  
+      const loggedInUser = req.user;
+      console.log(loggedInUser);
+  
+      Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+  
+      await loggedInUser.save();
+  
+      res.json({
+        message: `${loggedInUser.firstName}, your profile updated successfuly`,
+        data: loggedInUser,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
+
+
+  
+  module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,25 +1,40 @@
-
-const validator = require('validator');
+const validator = require("validator");
 
 const validateSignupData = (req) => {
-        const {firstName, lastName, email, password} = req.body; 
+  const { firstName, lastName, email, password } = req.body;
 
-        if(!firstName || !lastName){
-            throw new Error("please enter firstName and lastName");            
-        }
-        else if(firstName.length < 4 || firstName.length > 50){
-            throw new Error("firstName should be between 4 and 50 characters long");
-        }
-        else if(lastName.length < 4 || lastName.length > 50){
-            throw new Error("lastName should be between 4 and 50 characters long");
-        }
-        else if(!email || !validator.isEmail(email)){
-            throw new Error("Please enter a valid email");
-        }
-        else if( !validator.isStrongPassword(password)){
-            throw new Error("Password should be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character");
-        }
+  if (!firstName || !lastName) {
+    throw new Error("please enter firstName and lastName");
+  } else if (firstName.length < 4 || firstName.length > 50) {
+    throw new Error("firstName should be between 4 and 50 characters long");
+  } else if (lastName.length < 4 || lastName.length > 50) {
+    throw new Error("lastName should be between 4 and 50 characters long");
+  } else if (!email || !validator.isEmail(email)) {
+    throw new Error("Please enter a valid email");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password should be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    );
+  }
+};
 
-}
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
 
-module.exports = {validateSignupData}  ;  
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
+module.exports = { validateSignupData, validateEditProfileData };
